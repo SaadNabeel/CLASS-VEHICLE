e0c287d729b26dbfe78ebd68befd1822b4138577
SaadNabeel <saadnabeel2010@gmail.com>
2024-10-27 14:53:15 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self, speed, mileage):
+        self.speed = speed
+        self.mileage = mileage
+
+        print('Mileage:', self.mileage)
+        print('Speed:', self.speed)
+
+v1 = Vehicle(200, 30)
+v2 = Vehicle(300, 32)
